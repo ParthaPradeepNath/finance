7168b6758890e42bedbf619dc69c6069412a0c74
ParthaPradeepNath <parthanath96134@gmail.com>
2025-09-28 18:29:15 +0530

    transactions form done

--- README.md
+++ README.md
@@ -34,3 +34,21 @@ You can check out [the Next.js GitHub repository](https://github.com/vercel/next
 The easiest way to deploy your Next.js app is to use the [Vercel Platform](https://vercel.com/new?utm_medium=default-template&filter=next.js&utm_source=create-next-app&utm_campaign=create-next-app-readme) from the creators of Next.js.
 
 Check out our [Next.js deployment documentation](https://nextjs.org/docs/app/building-your-application/deploying) for more details.
+
+Storing currency in Databases
+
+1. Float/Double (no go, they do not offer precision they might look good and easy to store it that way but they are not precise when the moment you start calculating they will turn into a bunch of errors)
+eg. 0.1 * 0.2 = 0.020000000000000004
+
+2. Decimal / Numeric ?
+ - But Numeric and Decimal does not exist in J.S
+ - Requires us to use additional libs
+ - Not cross-language compatible (Requires to change the stack and find a way to support the type in the databse also)
+- Create problem in end to end type safety specially when it comes to forms
+
+3. come up with a solution
+
+Using integers of the smallest unit of the currency
+-We are going to use milinuts to support 3 decimals 
+- cross-language compatible
+- $10.50 => 10500
--- db/schema.ts
+++ db/schema.ts
@@ -1,4 +1,6 @@
-import { pgTable, text } from "drizzle-orm/pg-core";
+import {z} from "zod";
+import { integer, pgTable, text, timestamp } from "drizzle-orm/pg-core";
+import { relations } from "drizzle-orm";
 import { createInsertSchema } from "drizzle-zod";
 
 export const accounts = pgTable("accounts", {
@@ -8,7 +10,11 @@ export const accounts = pgTable("accounts", {
   userId: text("user_id").notNull(),
 });
 
-export const insertAccountSchema = createInsertSchema(accounts)
+export const accountsRelations = relations(accounts, ({ many }) => ({
+  transactions: many(transactions),
+}));
+
+export const insertAccountSchema = createInsertSchema(accounts);
 
 export const categories = pgTable("categories", {
   id: text("id").primaryKey(),
@@ -17,4 +23,39 @@ export const categories = pgTable("categories", {
   userId: text("user_id").notNull(),
 });
 
-export const insertCategorySchema = createInsertSchema(categories)
+export const categoriesRelations = relations(categories, ({ many }) => ({
+  transactions: many(transactions),
+}));
+
+export const insertCategorySchema = createInsertSchema(categories);
+
+export const transactions = pgTable("transactions", {
+  id: text("id").primaryKey(),
+  amount: integer("amount").notNull(),
+  payee: text("payee").notNull(),
+  notes: text("notes"),
+  date: timestamp("date", { mode: "date" }).notNull(),
+  accountId: text("account_id")
+    .references(() => accounts.id, {
+      onDelete: "cascade",
+    })
+    .notNull(),
+  categoryId: text("category_id").references(() => categories.id, {
+    onDelete: "set null",
+  }),
+});
+
+export const transactionsRelations = relations(transactions, ({ one }) => ({
+  account: one(accounts, {
+    fields: [transactions.accountId],
+    references: [accounts.id],
+  }),
+  categories: one(categories, {
+    fields: [transactions.categoryId],
+    references: [categories.id],
+  }),
+}));
+
+export const insertTransactionSchema = createInsertSchema(transactions, {
+  date: z.coerce.date(),
+});
